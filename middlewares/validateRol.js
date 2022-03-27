4f951b2b8ef950ad2b01634960a50339ca41cb0e
OrlandoMR <orlandomoncada610@gmail.com>
2022-03-27 16:22:32 -0500

    Miidleware rol

--- middlewares/validateRol.js
+++ middlewares/validateRol.js
@@ -11,7 +11,7 @@ const isAdminRole = (req = request, res = response, next) => {
 
     const {rol,nombre}=req.usuario;
 
-    if(rol !== 'ADMIN_ROLE'){
+    if(rol !== 'ADMIN_ROL'){
         return res.status(401).json({
             msg: `${nombre} no es administrador`
         })
