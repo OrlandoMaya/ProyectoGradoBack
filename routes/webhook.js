147f7bae3c6cfbd999fc1528c6f87897f6d494e3
LotusZaheer <andresfelipeuribe11@gmail.com>
2022-05-22 10:06:56 -0500

    Merge branch 'main' of https://github.com/OrlandoMR/ProyectoGradoBack

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,15 +1,17 @@
-const { Router } = require('express');
-const { saveInfo, getRecursos } = require('../controllers/webhook.controller');
-const {login}=require('../controllers/auth.controller')
-const { check } = require('express-validator');
-const { validateForm } = require('../middlewares/validateField');
-const { validateJWT } = require('../middlewares/validateJWT');
-const { isAdminRole } = require('../middlewares/validateRol');
+const { Router } = require("express");
+const { saveInfo, getRecursos } = require("../controllers/webhook.controller");
+const { controlesGet } = require("../controllers/control.controller");
+const { login } = require("../controllers/auth.controller");
+const { check } = require("express-validator");
+const { validateForm } = require("../middlewares/validateField");
+const { validateJWT } = require("../middlewares/validateJWT");
+const { isAdminRole } = require("../middlewares/validateRol");
 const router = Router();
 
-router.post('/saver-webhook', [
-], saveInfo)
+router.post("/saver-webhook", [], saveInfo);
 
-router.get('/resource',[],getRecursos)
+router.get("/resource", [], getRecursos);
+
+router.get("/", [], controlesGet);
 
 module.exports = router;
