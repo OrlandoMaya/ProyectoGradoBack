3247c516ff7f8a54141d5937937fdfb930afc31c
OrlandoMR <orlandomoncada610@gmail.com>
2022-06-12 23:06:14 -0500

    add check

--- routes/estacion.js
+++ routes/estacion.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const { estacionGetByTopic, estacionGet,estacionesGet,estacionPost,estacionPut,estacionDelete } = require('../controllers/estacion.controller');
 const { validateJWT } = require('../middlewares/validateJWT');
+const { check } = require('express-validator');
 const router = Router();
 
 router.get('/:id', [
